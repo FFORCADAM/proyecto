9a80b177e66eae23680c1d0124f77d3453e3876f
Agr1ax <91204003+Agr1ax@users.noreply.github.com>
2022-05-04 22:23:52 +0200

    Update servidor.py
    A priori así funciona el servidor como toca, estaría acabado

--- servidor.py
+++ servidor.py
@@ -1,16 +1,11 @@
 import socket
 import time
-import rsa                        
-from rsa import cli, PrivateKey   
-from rsa import key               
-from rsa import prime             
-from rsa import common            
+from rsa import key
 
-pub,priv=key.newkeys(2048)      
-print(pub) #Devuelve n,e        
-print(priv) #Devuelve n,e,d,p,q 
 
 header=1024
+pub,priv=key.newkeys(2048)
+
 misocket= socket.socket()
 misocket.bind(('localhost', 8000))
 misocket.listen(5)
@@ -18,24 +13,25 @@ formato= 'utf-8'
 while True:
     conexion, addr = misocket.accept()
     print("Conexión establecida")
-    firma_length= conexion.recv(header)
+    conexion.send(pub.e.to_bytes(byteorder="big", length=1024))
+
+    x_length= conexion.recv(header)
     time.sleep(5)
-    if firma_length:
+    if x_length:
         print("recibida longitud firma")
-        firma_length=int.from_bytes(firma_length, byteorder="big")
+        x_length=int.from_bytes(x_length, byteorder="big")
         time.sleep(5)
-        print(firma_length)
-        firma = int.from_bytes(conexion.recv(firma_length), byteorder="big")
+        print(x_length)
+        x = int.from_bytes(conexion.recv(x_length), byteorder="big")
         time.sleep(5)
-        print(firma)
+        print(x)
         print("recibida firma")
-        d=int.from_bytes(conexion.recv(header), byteorder="big")
+        d=priv.d
         time.sleep(5)
-        print("recibida d:"+ str(d))
         n=int.from_bytes(conexion.recv(header), byteorder="big")
         time.sleep(5)
         print("recibida n:" + str(n))
-        firmado= (firma^d)%n #creo que así se calcula la firma, siendo el "2" el elemento d que no se cual es.
+        firmado= (x^d)%n #creo que así se calcula la firma, siendo el "2" el elemento d que no se cual es.
         time.sleep(5)
-        conexion.send(firmado.to_bytes(byteorder="big", length=256))
+        conexion.send(firmado.to_bytes(byteorder="big", length=1024))
         conexion.close()
