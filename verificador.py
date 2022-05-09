752b10f758435ff2dbf0ad1bfbcaed3340f393dd
Agr1ax <91204003+Agr1ax@users.noreply.github.com>
2022-05-10 00:12:14 +0200

    Update verificador.py

--- verificador.py
+++ verificador.py
@@ -1,11 +1,6 @@
 import hashlib
-import socket
-import time
-
 from rsa import PublicKey
 
-
-import rsa
 def getsha256file(archivo):
     try:
         hashsha = hashlib.sha256()
@@ -22,35 +17,18 @@ def getsha256file(archivo):
 
 archivo=input("Introduce el nombre del archivo que quieres verificar:")
 ffirma=open(input("Introduce el fichero que tiene la firma:"), encoding="latin-1")
-#header=1024
-'''
-versocket= socket.socket()
-versocket.connect(('localhost', 8000))
-
-
-time.sleep(5)
-
-
-
-d=int.from_bytes(versocket.recv(header), byteorder="big")
-e=int.from_bytes(versocket.recv(header), byteorder="big")
-time.sleep(5)
-n=int.from_bytes(versocket.recv(header), byteorder="big")
-time.sleep(5)
-'''
-
-pubkey=PublicKey._load_pkcs1_pem(open("pubKey.pem", "rb").read())
+clavepub=open("pubKey.pem", "rb")
+pubkey=PublicKey._load_pkcs1_pem(clavepub.read())
 e=pubkey.e
 n=pubkey.n
 firma=(int.from_bytes(bytes(ffirma.readline(), encoding="latin-1"), byteorder="big"))%n
 ffirma.close()
 hashh=(int(getsha256file(archivo), base=16))%n
-#x_length=0
-#versocket.send(x_length.to_bytes(length=1024, byteorder="big"))
-if hashh==(firma**e)%n:
+comparacion=(firma**e)%n
+if hashh==comparacion:
     print("La firma es válida")
 else:
     print("La firma no es válida")
     print(hashh)
-    print(len(str(hashh)))
-#versocket.close()
+    print(comparacion)
+clavepub.close()
