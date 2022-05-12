3bf953746e2151bf71198f302d35d02790bbbebc
FFORCADAM <91393949+FFORCADAM@users.noreply.github.com>
2022-05-12 02:59:18 +0200

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -7,13 +7,13 @@ pub,priv=key.newkeys(2048)
 
 try:
     pubkey=PublicKey._load_pkcs1_pem(open("pubKey.pem", "rb").read())
-    print("Obtenemos del archivo pubkey.pem la clave pública")
+    print("Obtenemos del archivo pubKey.pem la clave pública")
     e=pubkey.e
     n=pubkey.n
     privkey=PrivateKey._load_pkcs1_pem(open("privKey.pem", "rb").read())
     d=privkey.d
 except IOError:
-    print('El servidor no se ha ejecutado nunca o se ha movido el fichero de la clave pública de sitio; procedemos a crear el archivo pubkey.pem para guardar la clave pública disponible aun con el servidor apagado ')
+    print('El servidor no se ha ejecutado nunca o se ha movido el fichero de la clave pública de sitio.',\n,'Procedemos a crear el archivo pubKey.pem para guardar la clave pública disponible aun con el servidor apagado ')
     with open("pubKey.pem", "wb") as q:
         q.write(PublicKey._save_pkcs1_pem(pub))
         e=pub.e
@@ -32,14 +32,14 @@ while True:
     x_length= int.from_bytes(conexion.recv(header), byteorder="big")
     time.sleep(1)
     if x_length:
-        print("recibida longitud firma")
+        print("recibida longitud firma de parte del cliente")
         time.sleep(1)
         x = int.from_bytes(conexion.recv(x_length), byteorder="big")
         time.sleep(1)
         print(x)
-        print("recibida firma")
+        print("Recibido el hash cegado")
         time.sleep(1)
-        print("Comienza el cálculo de firma")
+        print("Realizamos la firma")
         firmado=pow(x,d,n)
         time.sleep(1)
         conexion.send(firmado.to_bytes(byteorder="big", length=1024))
