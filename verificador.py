4cf6da01854aabc970e0577ba50c1f61c5c5acd2
FFORCADAM <91393949+FFORCADAM@users.noreply.github.com>
2022-05-12 12:19:31 +0200

    Update verificador.py

--- verificador.py
+++ verificador.py
@@ -16,15 +16,23 @@ def getsha256file(archivo):
         return ""
 
 archivo=input("Introduce el nombre del archivo que quieres verificar:")
-ffirma=open(input("Introduce el fichero que tiene la firma:"), encoding="latin-1")
+ffirma=input("Introduce el fichero que tiene la firma:")
 clavepub=open("pubKey.pem", "rb")
 pubkey=PublicKey._load_pkcs1_pem(clavepub.read())
 e=pubkey.e
 n=pubkey.n
-firma=(int.from_bytes(bytes(ffirma.read().lstrip(), encoding="latin-1"), byteorder="big"))%n
-ffirma.close()
+
 hashh=(int(getsha256file(archivo), base=16))%n
-comparacion=pow(firma,e,n)
+with open(ffirma,"r",encoding='latin-1') as f:
+    firma=""
+    for readline in f:
+        lineastrip=readline
+        firma +=lineastrip
+
+#firma=(int.from_bytes(bytes(ffirma.read().lstrip(), encoding="latin-1"), byteorder="big"))%n
+#ffirma.close()
+hashh=(int(getsha256file(archivo), base=16))%n
+comparacion=pow(int(firma),e,n)%n
 if hashh==comparacion:
     print("La firma corresponde al fichero de entrada")
 else:
@@ -32,4 +40,3 @@ else:
     #print(hashh)
     #print(comparacion)
 clavepub.close()
-
